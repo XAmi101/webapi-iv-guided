f6740a6b3f52dd4e7790451fa158527768fc46c9
XAmi101 <famousvacancy@gmail.com>
2019-10-31 13:23:15 -0400

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const server = require('./api/server.js');
 require("dotenv").config()
 
-const port = 9000;
+const port = process.env.PORT || 9990 ;
 
 server.listen(port, () => {
   console.log('\n*** Server Running on http://localhost:${port} ***\n');
